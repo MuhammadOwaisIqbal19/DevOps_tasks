b6b87d8bb36a39a5528db3f3c1b010e96a6cc469
MuhammadOwaisIqbal19 <imowais728@gmail.com>
2025-09-18 15:49:48 +0500

    eslint.config.js updated

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,7 @@
-export default [
+module.exports = [
   {
     files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module"
-    },
+    languageOptions: { ecmaVersion: "latest" },
     rules: {
       semi: "error",
       quotes: ["error", "double"]
